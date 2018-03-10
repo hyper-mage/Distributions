3271d1a2afbbb0529127b92b6e1b5647866189f7
hyper-mage <mlynn808138@gmail.com>
2018-03-10 11:43:08 -0600

    Add files via upload

---
+++ Distribution.py
@@ -0,0 +1,7 @@
+#This file is for bettering my skills in python as well as statistical distributions
+
+class Distribution(object):
+    #Constructor for object
+    def __init__(self, discrete, continuous):
+        self.discrete = discrete
+        self.continuous = continuous
